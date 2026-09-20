ead8517e81b636cfdc2b1a21dbd2e81c2cb20901
ELY M. <elymbmx@gmail.com>
2026-09-20 04:39:46 -0500

    ownstyle

--- src/modules/ownstyle/libkviownstyle.cpp
+++ src/modules/ownstyle/libkviownstyle.cpp
@@ -1,18 +1,59 @@
 #include "KviApplication.h"
-#include "KviCString.h"
+#include "KviLocale.h"
 #include "KviModule.h"
+#include "KviWindow.h"
+#include "kvi_out.h"
 
+
+static bool ownstyle_cmd_about(KviKvsModuleCommandCall * c)
+{
+
+	c->window()->output(KVI_OUT_SYSTEMMESSAGE, __tr2qs_ctx("ownstyle module by ELY M.", "ownstyle"));
+	return true;
+}
+
+static bool ownstyle_cmd_run(KviKvsModuleCommandCall * c)
+{
+
+	c->window()->output(KVI_OUT_SYSTEMMESSAGE, __tr2qs_ctx("ownstyle module loaded and changed the colors :)", "ownstyle"));
+	if (g_pApp) {
+		QString styleSheet = "* { color: black; background-color: #959CBF; } "
+		                     "QTextEdit { background: #E2E2E2; } "
+		                     "QLineEdit { background: #E2E2E2; } "
+		                     "QListView { background: #E2E2E2; } "
+		                     "QListWidget { background: #E2E2E2; } "
+		                     "QMessageBox { background: #E2E2E2; } "
+		                     "QSpinBox { background: #E2E2E2; } "
+		                     "QTreeView { background: #E2E2E2; } "
+		                     "QCheckBox { background: #E2E2E2; }";
+		g_pApp->setStyleSheet(styleSheet);    
+	} 	
+		
+	return true;
+}
 
 
 static bool ownstyle_module_init(KviModule * m)
 {
 
-	KviApplication * pTheApp;
-	QString styleSheet = "* { color: black; background-color: #959CBF; } QTextEdit { background: #E2E2E2; } QLineEdit { background: #E2E2E2; } QListView { background: #E2E2E2; } QListWidget { background: #E2E2E2; } QMessageBox { background: #E2E2E2; } QSpinBox { background: #E2E2E2; } QTreeView { background: #E2E2E2; } QCheckBox { background: #E2E2E2; }";
-	pTheApp->setStyleSheet(styleSheet);    
-    
+	KVSM_REGISTER_SIMPLE_COMMAND(m, "about", ownstyle_cmd_about);
+	KVSM_REGISTER_SIMPLE_COMMAND(m, "run", ownstyle_cmd_run);
+	
+	if (g_pApp) {
+		QString styleSheet = "* { color: black; background-color: #959CBF; } "
+		                     "QTextEdit { background: #E2E2E2; } "
+		                     "QLineEdit { background: #E2E2E2; } "
+		                     "QListView { background: #E2E2E2; } "
+		                     "QListWidget { background: #E2E2E2; } "
+		                     "QMessageBox { background: #E2E2E2; } "
+		                     "QSpinBox { background: #E2E2E2; } "
+		                     "QTreeView { background: #E2E2E2; } "
+		                     "QCheckBox { background: #E2E2E2; }";
+		g_pApp->setStyleSheet(styleSheet);    
+	}    
 	return true;
 }
+
 
 static bool ownstyle_module_cleanup(KviModule *)
 {
